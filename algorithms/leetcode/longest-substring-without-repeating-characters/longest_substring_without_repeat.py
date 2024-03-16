6e4b6bd5a1f3406afab68e63348cd2a9499fe430
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-16 12:53:26 -0400

    add users solutions

--- algorithms/leetcode/longest-substring-without-repeating-characters/longest_substring_without_repeat.py
+++ algorithms/leetcode/longest-substring-without-repeating-characters/longest_substring_without_repeat.py
@@ -61,7 +61,7 @@ class Solution(unittest.TestCase):
             else:
                 i += 1
         return length
-
+    
     def test_1(self):
         self.assertEqual(self.self_soln("abcabcbb"), 3)
 
@@ -70,6 +70,19 @@ class Solution(unittest.TestCase):
 
     def test_3(self):
         self.assertEqual(self.self_soln("pwwkew"), 3)
+
+class UserSolutions(unittest.TestCase):
+    def user_sol_1(self, s: str) -> int:
+        left = max_length = 0
+        char_set = set()
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        return max_length
 
 if __name__ == "__main__":
     unittest.main()
