912e9bc95d92864ee35c4358b38d90762872dcdb
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-18 17:19:41 -0400

    updated codewars don't give me five implementation in ts

--- algorithms/codewars/dont-give-me-five/dont_give_me_five.ts
+++ algorithms/codewars/dont-give-me-five/dont_give_me_five.ts
@@ -1,7 +1,7 @@
 export function dont_give_me_five(start: number, end: number): number {
     let count = 0;
     for (let i = start; i <= end; i++) {
-        if (i.toString().indexOf('5') === -1) {
+        if (!String(i).match('5')) {
             count++;
         }
     }
