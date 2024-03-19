34c19f1d8cc1fe24a655cba3d08e96767110bdc8
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-19 14:52:24 -0400

    add linked list header for c

---
+++ data-structures/c/include/linked_list/linked_list.h
@@ -0,0 +1,26 @@
+/*
+@gitsil10
+S Flores
+
+@date
+2024-03
+
+@title
+Linked List
+
+@brief
+Linked list header file
+
+@note
+time: O(n)
+space: O(n)
+
+@details
+linked list header file
+node -> struct | integer data | pointer to the next node
+create -> creates a new node with the given data
+insert -> inserts a new node with the given data at the head of the list
+delete -> deletes the first node with the given data
+print -> prints the list
+free -> frees the list | clear memory
+*/
