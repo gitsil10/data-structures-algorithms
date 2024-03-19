9844a536d80e27019d0dcaef34f00b310e3981d0
gitsil10 <105307487+gitsil10@users.noreply.github.com>
2024-03-19 15:25:02 -0400

    update linked list header for c add create linked list update main add simple implementation

--- data-structures/c/src/main.c
+++ data-structures/c/src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/linked_list/linked_list.h"
 
-
+#define MAX 10
 int main(int argc, char **argv) {
+    int arr[MAX];
+    for (int i = 0; i < MAX; i++) {
+        arr[i] = i;
+    }
+    node *lists = create_linked_list(arr, MAX);
 
+    while (lists) {
+        printf("%d ", lists->data);
+        lists = lists->next;
+    }
 
     return 0;
 }
